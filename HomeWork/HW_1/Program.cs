4045b49e615d46c9fa45f0c1dda8824ae11586a1
Mary <mariyastch@gmail.com>
2023-09-18 15:45:58 +0300

    1, 2 tasks done

---
+++ HomeWork/HW_1/Program.cs
@@ -0,0 +1,46 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// System.Console.WriteLine("Input number A: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input number B: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if(a>b)
+// {
+//     System.Console.WriteLine($"max {a}");
+// }
+// else
+// {
+//     System.Console.WriteLine($"max: {b}");
+// }
+
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// System.Console.WriteLine("Input number A: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input number B: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input number C: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// int max = a;
+
+// if (b>max) max = b;
+// if (c>max) max = c;
+
+// System.Console.WriteLine($"max: {max}");
+
+
+
+
+
+
+
